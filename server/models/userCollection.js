58e6c2c40dbdd278ecb78afbf68148ab8c3afeb4
amir-skoric <89415335+amir-skoric@users.noreply.github.com>
2023-11-16 18:26:36 +0100

    added validation to user schema

--- server/models/userCollection.js
+++ server/models/userCollection.js
@@ -12,10 +12,19 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
+    //Regex function to test if e-mail address includes @-symbol
+    validate: {
+      validator: function (value) {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+      },
+      message: "E-mail address is invalid",
+    },
   },
   password: {
     type: String,
     required: true,
+    minLength: 8,
   },
 });
 
