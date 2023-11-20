a5b226de63e8c682b93f4e8d16cad8abd88d1642
amir-skoric <89415335+amir-skoric@users.noreply.github.com>
2023-11-20 15:09:57 +0100

    added spinner if loading from database

--- client/src/components/Webshops/Webshops.jsx
+++ client/src/components/Webshops/Webshops.jsx
@@ -2,17 +2,17 @@
 import React, { useEffect, useState } from "react";
 import "./Webshops.css";
 import axios from "axios";
+import Spinner from "../Spinner/Spinner";
 
 const WebshopTable = () => {
-
   const [webshops, setWebshops] = useState({});
- const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
   async function getWebshops() {
     try {
       await axios.get("http://localhost:4000/getWebshops").then((res) => {
         if (res.status === 200) {
-          setWebshops(res.data)
-          setLoading(false)
+          setWebshops(res.data);
+          setLoading(false);
         }
       });
     } catch (error) {
@@ -26,9 +26,13 @@ const WebshopTable = () => {
 
   return (
     <div className="webshopTable">
-      {!loading && webshops.map((item) => {
-        return <p key={item._id}>{item.name}</p>
-      })}
+      {!loading ? (
+        webshops.map((item) => {
+          return <p key={item._id}>{item.name}</p>;
+        })
+      ) : (
+        <Spinner />
+      )}
     </div>
   );
 };
