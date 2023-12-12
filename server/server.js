dee0e885a179ee775dd12400632e9f11ef8a8e44
amir-skoric <89415335+amir-skoric@users.noreply.github.com>
2023-12-12 04:40:19 +0100

    very small change to port handling

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 const mongo = require("./mongo");
 const cors = require("cors");
 const session = require("express-session");
