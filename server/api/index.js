1381443e2a268453fd0abcc59fdf47abbf783eaa
amir-skoric <89415335+amir-skoric@users.noreply.github.com>
2023-12-13 19:02:11 +0100

    cookie set to lax

--- server/api/index.js
+++ server/api/index.js
@@ -29,7 +29,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       httpOnly: true,
-      sameSite: "none",
+      sameSite: "lax",
       secure: true,
       maxAge: 60 * 20 * 1000, //expires after 20 minutes
     },
